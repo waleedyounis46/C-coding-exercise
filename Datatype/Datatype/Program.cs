aa87a9be83bdcf859f4e0cd5c2ea748ff8242db7
waleedyounis46 <102731689+waleedyounis46@users.noreply.github.com>
2022-04-03 16:37:50 +0100

    Data Types

---
+++ Datatype/Datatype/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datatype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int:{0}", sizeof(int));
+            Console.WriteLine("Size of long:{0}", sizeof(long));
+            Console.WriteLine("Size of double:{0}", sizeof(double));
+            Console.WriteLine("size of boolean):{0}", sizeof(bool));
+            Console.WriteLine("Size of char: {0}", sizeof(char));
+            Console.WriteLine("size of byte: {0}", sizeof(byte));
+            Console.ReadLine();
+        }
+    }
+}

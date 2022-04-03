9a7c5996a279709ed6477e44f7f70f29be14d9e6
waleedyounis46 <102731689+waleedyounis46@users.noreply.github.com>
2022-04-03 16:34:51 +0100

    Hello World

---
+++ if_else/if_else/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 10;
+            int lastNumber = 08;
+            //Console.WriteLine("Enter first Number:");
+            //firstNumber = Convert.ToInt16(Console.ReadLine());
+            //Console.WriteLine("Enter last number:");
+            //lastNumber = Convert.ToInt16(Console.ReadLine());
+            if (firstNumber > lastNumber)
+            {
+                Console.WriteLine("First Number is greater than last number");
+            }
+            else if (firstNumber == lastNumber)
+            {
+                Console.WriteLine("last number is greate than first number");
+             }
+            Console.ReadLine();
+        }
+    }
+}

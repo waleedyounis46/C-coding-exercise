b1a08d35bee8f752edf70541d9c2907ae0d81cd0
waleedyounis46 <102731689+waleedyounis46@users.noreply.github.com>
2022-04-03 17:41:32 +0100

    Loops

---
+++ loops/loops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //sum of squares
+            double sum = 0;
+            double count = 0;
+            do
+            {
+                sum = sum + Math.Pow(count, 2.0);
+                Console.WriteLine("count: {0} sum: {1}", sum, count);
+                count++;
+            }
+            while (count != 10);
+            Console.ReadLine();
+        }
+    }
+}
